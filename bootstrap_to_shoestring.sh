972a49d02e1adfbe798cdde2ad5a3a20ebf4d22d
mikun <45915246+mikunNEM@users.noreply.github.com>
2025-06-12 01:37:56 +0900

    Update bootstrap_to_shoestring.sh

--- bootstrap_to_shoestring.sh
+++ bootstrap_to_shoestring.sh
@@ -681,8 +681,7 @@ setup_shoestring() {
         mv "$overrides_file" "$overrides_file.bak-$(date +%Y%m%d_%H%M%S)"
         print_info "既存の overrides.ini をバックアップ: $overrides_file.bak-$(date +%Y%m%d_%H%M%S)"
     fi
-    cat > "$overrides_file" << 'EOF'
-
+cat > "$overrides_file" << 'EOF'
 [user.account]
 enableDelegatedHarvestersAutoDetection = true
 
@@ -701,7 +700,7 @@ EOF
     sed -i "s/\$host_name/$host_name/g" "$overrides_file"
     sed -i "s/\$friendly_name/$friendly_name/g" "$overrides_file"
     
-    #log "overrides.ini 内容: $(cat "$overrides_file" | sed 's/["'"]/\\&/g')" "DEBUG"
+    log "overrides.ini 内容: $(cat "$overrides_file" | sed 's/["'"]/\\&/g')" "DEBUG"
     validate_ini "$overrides_file"
     if ! $SKIP_CONFIRM; then
         confirm_and_edit_ini "$overrides_file"
